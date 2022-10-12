7fdd152a883e2ba4afda4cf6bfa8f66bd4bb4ff8
Abhishek Kumar <aplusk314@gmail.com>
2022-10-12 21:16:33 +0530

    initial commit

---
+++ app/root.tsx
@@ -0,0 +1,59 @@
+import type { MetaFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+  useLoaderData,
+} from "@remix-run/react";
+
+import globalStylesUrl from './styles/global.css'
+import favIconUrl from '../public/favicon.ico';
+
+export const meta: MetaFunction = () => ({
+  charset: "utf-8",
+  title: "aplusk.dev | Home",
+  viewport: "width=device-width,initial-scale=1",
+});
+
+export const links = () => [
+  { rel: 'stylesheet', href: globalStylesUrl },
+  { rel: 'icon', href: favIconUrl }
+]
+
+export async function loader() {
+  return json({
+    ENV: {
+      NODE_ENV: process.env.NODE_ENV,
+    },
+  });
+}
+
+export default function App() {
+  const data = useLoaderData();
+
+  return (
+    <html lang="en" data-theme="light">
+      <head>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.ENV = ${JSON.stringify(
+              data.ENV
+            )}`,
+          }}
+        />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
---
+++ app/routes/index.tsx
@@ -0,0 +1,5 @@
+export default function Index() {
+  return (
+    <h1>aplusk.dev</h1>
+  );
+}
